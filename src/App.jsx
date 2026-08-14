899ae436f4421e3759170fc3b7280337342a7bf2
anmol-singh-chehal <devanmolsinghchehal@gmail.com>
2026-08-14 22:46:55 +0530

    created modatlities section and created model card component

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ export default function App() {
       </header>
       <main>
         <Home/>
+
       </main>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Badge from '@/components/Badge'
 import { ArrowRight } from 'lucide-react'
 import homeImageLight from "@/assets/homeImageLight.jpg"
+import ModelCard from '@/components/ModelCard'
 
 export default function Home() {
   return (
@@ -20,11 +21,11 @@ export default function Home() {
           </h3>
 
           <div className='flex lg:gap-4 sm:gap-4'>
-            <button className='font-medium bg-muted text-white flex lg:gap-2 items-center lg:px-4 lg:py-2 lg:text-lg rounded-md cursor-pointer ring-2 ring-muted sm:text-sm sm:px-2 sm:py-2 sm:gap-1'>
+            <button className='font-medium bg-muted text-white flex lg:gap-2 items-center lg:px-4 lg:py-2 lg:text-lg rounded-md cursor-pointer ring-2 ring-muted sm:text-sm sm:px-2 sm:py-2 sm:gap-1 font-primary'>
               <h3>Try Image Detection</h3>
               <ArrowRight className='sm:size-4'/>
             </button>
-            <button className='font-medium text-muted flex lg:gap-2 items-center lg:px-4 lg:py-2 lg:text-lg rounded-md ring-2  hover:bg-muted hover:text-white hover:ring-2 hover:ring-muted bg-muted/10 ring-muted/40 cursor-pointer sm:text-sm sm:px-2 sm:py-2 sm:gap-1'>Learn About Us</button>
+            <button className='font-medium text-muted flex lg:gap-2 items-center lg:px-4 lg:py-2 lg:text-lg rounded-md ring-2  hover:bg-muted hover:text-white hover:ring-2 hover:ring-muted bg-muted/10 ring-muted/40 cursor-pointer sm:text-sm sm:px-2 sm:py-2 sm:gap-1 font-primary'>Learn About Us</button>
           </div>
         </div>
 
@@ -34,6 +35,23 @@ export default function Home() {
         </div>
 
       </section>
+
+      <section className='sm:py-10 sm:px-4 lg:px-8 lg:py-20 xl:px-12 flex flex-col sm:gap-10'>
+       <div className='flex flex-col text-center sm:gap-2'>
+          <h1 className='sm:text-xl md:text-2xl lg:text-3xl font-medium text-primary font-primary'>Explore Multiple Imaging Modalities</h1>
+          <p className='sm:text-sm lg:text-lg font-medium text-secondary font-secondary'>Our platform supports various scan types, providing tailored AI analysis workflows for different research needs.</p>
+       </div>
+
+        <div className='flex flex-col sm:gap-4 lg:grid lg:grid-cols-2 xl:grid-cols-3'>
+          <ModelCard faIcon="fa-brain" title={"Model Efficient-Net-b0"} desc={"Our platform supports various scan types, providing tailored AI analysis workflows for different research needs."}/>
+          <ModelCard faIcon="fa-brain" title={"Model Efficient-Net-b0"} desc={"Our platform supports various scan types, providing tailored AI analysis workflows for different research needs."}/>
+          <ModelCard faIcon="fa-brain" title={"Model Efficient-Net-b0"} desc={"Our platform supports various scan types, providing tailored AI analysis workflows for different research needs."}/>
+          <ModelCard faIcon="fa-brain" title={"Model Efficient-Net-b0"} desc={"Our platform supports various scan types, providing tailored AI analysis workflows for different research needs."}/>
+          <ModelCard faIcon="fa-brain" title={"Model Efficient-Net-b0"} desc={"Our platform supports various scan types, providing tailored AI analysis workflows for different research needs."}/>
+          <ModelCard faIcon="fa-brain" title={"Model Efficient-Net-b0"} desc={"Our platform supports various scan types, providing tailored AI analysis workflows for different research needs."}/>
+        </div>
+      </section>
+
 
     </main>
   )
