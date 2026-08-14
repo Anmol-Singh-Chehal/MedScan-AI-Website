d255422a42f40867663624d1c34efebeee5cc847
anmol-singh-chehal <devanmolsinghchehal@gmail.com>
2026-08-14 19:31:08 +0530

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <div className='bg-amber-500 text-2xl font-semibold'>
-      Hello Bro.
-    </div>
+    <header>
+      <Navbar/>
+    </header>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Sun, BriefcaseMedical } from "lucide-react";
+import { Button } from '@base-ui/react';
+
+export default function Navbar() {
+  return (
+    <nav className='flex justify-between sm:px-4 sm:py-4 lg:py-0 shadow-[0_2px_12px_rgba(0,0,0,0.12)] '>
+      
+      <div className='flex sm:gap-1 justify-center items-center'>
+        <BriefcaseMedical className='text-muted sm:size-6'/>
+        <h1 className='text-muted sm:text-lg font-semibold'>MedScan AI</h1>
+      </div>
+
+      <ul className='hidden lg:flex md:gap-8'>
+        <li className='font-medium border-b-4 border-muted text-muted py-4 cursor-pointer hover:border-muted/30'>Home</li>
+        <li className='font-medium py-4 cursor-pointer hover:border-b-4 border-muted/30'>About Us</li>
+        <li className='font-medium py-4 cursor-pointer hover:border-b-4 border-muted/30'>Detection</li>
+        <li className='font-medium py-4 cursor-pointer hover:border-b-4 border-muted/30'>Contact Us</li>
+      </ul>
+
+      <div className='flex sm:gap-2 items-center justify-center'>
+        <button className='sm:size-7 hover:bg-muted/10 ring-1 rounded-full flex items-center justify-center hover:cursor-pointer text-muted'> 
+          <Sun className='sm:size-4 text-muted'/> 
+        </button>
+        <Button className={"btn-2"}>Login</Button>
+        <Button className={"btn-1"}>Sign Up</Button>
+      </div>
+
+    </nav>
+  )
+}
